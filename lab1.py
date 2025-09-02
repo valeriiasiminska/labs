691246708603f7094fe1e914aaeb6cc4eb588e63
valeriiasiminska <stas.agapov.killer@gmail.com>
2025-09-02 23:10:20 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,20 @@
+age = 19
+weight = 60.2
+list_fruits = ['apple', 'orange','lemon']
+list_cars = ['amr','mercedes','audi']
+tuple_digits = (1,2,3,4,5,6,7,8,9,0)
+it_is_python = True
+unique_numbers = {14,87,94,881}
+height = 174
+students_in_group = 34
+year = 2025
+print(age)
+print(weight)
+print(list_fruits)
+print(list_cars)
+print(tuple_digits)
+print(it_is_python)
+print(unique_numbers)
+print(height)
+print(students_in_group)
+print(year)
